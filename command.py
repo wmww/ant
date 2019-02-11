c9b9823c4e025d0ddf92201a413896f30439c6eb
William Wold <wm@wmww.sh>
2019-02-11 12:40:00 -0500

    Fix command timout issue

--- command.py
+++ command.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import signal
 import cyberstate as cs
 
 class Result:
@@ -32,23 +34,41 @@ class TimoutError(Error):
     def __str__(self):
         return '`' + ' '.join(arg_list) + '` timed out after ' + self.time + ' seconds'
 
-def run(arg_list, timout=1, interactive=False, path=None, stdin_text=None, raise_if_fail=False):
+def run(arg_list, timout=1, interactive=False, cwd=None, stdin_text=None, raise_if_fail=False):
     """
     Run a command, returning a result
     arg_list: a list of strings, the command to run followed by the arguments
     timout: seconds
     interactive: if true, stdout/stderr will not be captured, and instead will get passed on directly to the user
-    path: working directory to run the command in
+    cwd: current working directory to run the command in
     stdin_text: input for the command
     raise_if_fail: if to raise a command.Error if the command fails
     """
     #log('Running `' + ' '.join(arg_list) + '`')
-    if interactive:
-        out_popen_arg = None
-    else:
-        out_popen_arg = subprocess.PIPE
-    p = subprocess.Popen(arg_list, cwd=path, stdout=out_popen_arg, stderr=out_popen_arg)
-    stdout, stderr = p.communicate(stdin_text)
+    out_popen_arg = None if interactive else subprocess.PIPE
+    try:
+        preexec_fn_popen_arg = os.setsid
+        kill_subprocess_tree_on_timout = True
+    except:
+        # if os.setsid is not available (Windows), we wont be able to kill the child process if it has children
+        # test_timout_actually_works_with_sh is the test that should fail
+        preexec_fn_popen_arg = None
+        kill_subprocess_tree_on_timout = False
+    p = subprocess.Popen(arg_list,
+                         cwd=cwd,
+                         stdout=out_popen_arg,
+                         stderr=out_popen_arg,
+                         preexec_fn=preexec_fn_popen_arg)
+    try:
+        stdout, stderr = p.communicate(stdin_text, timout)
+        did_timout = False
+    except subprocess.TimeoutExpired:
+        if kill_subprocess_tree_on_timout:
+            os.killpg(os.getpgid(p.pid), signal.SIGKILL)
+        else:
+            p.kill() # will usually work, but will block if the process has running children
+        stdout, stderr = p.communicate()
+        did_timout = True
     if not interactive:
         stdout = stdout.decode('utf-8') if stdout != None else ''
         stderr = stderr.decode('utf-8') if stderr != None else ''
@@ -57,6 +77,8 @@ def run(arg_list, timout=1, interactive=False, path=None, stdin_text=None, raise
         stderr = None
     exit_code = p.returncode
     result = Result(stdout, stderr, exit_code)
+    if did_timout:
+        raise TimoutError(arg_list, result, timout)
     if raise_if_fail and exit_code != 0:
         raise FailError(arg_list, result)
     return Result(stdout, stderr, exit_code)
--- test/test_command.py
+++ test/test_command.py
@@ -20,6 +20,12 @@ class TestCommand(unittest.TestCase):
         self.assertEqual(result.stderr, None)
         self.assertEqual(result.exit_code, 0)
 
+    def test_cwd(self):
+        current = command.run(['ls'], cwd='.', raise_if_fail=True)
+        dev = command.run(['ls'], cwd='/dev', raise_if_fail=True)
+        self.assertNotEqual(current.stdout, dev.stdout)
+        self.assertIn('null', {i: None for i in dev.stdout.split()})
+
     def test_false_result(self):
         result = command.run(['false'])
         self.assertEqual(result.exit_code, 1)
@@ -33,10 +39,45 @@ class TestCommand(unittest.TestCase):
         self.assertEqual(e.result.stderr, '')
         self.assertEqual(e.result.exit_code, 1)
 
+    def test_sleep_short(self):
+        result = command.run(['sleep', '0.05'], timout=0.1)
+        self.assertEqual(result.stdout, '')
+        self.assertEqual(result.stderr, '')
+        self.assertEqual(result.exit_code, 0)
+
     def test_sleep_timout(self):
         with self.assertRaises(command.TimoutError) as cm:
-            command.run(['sleep', '1'], timout=0.25)
+            command.run(['sleep', '1'], timout=0.05)
         e = cm.exception
-        self.assertEqual(result.stdout, None)
-        self.assertEqual(result.stderr, None)
-        self.assertEqual(e.result.exit_code, 1)
+        self.assertEqual(e.result.stdout, '')
+        self.assertEqual(e.result.stderr, '')
+        self.assertEqual(e.result.exit_code, -9)
+
+    def test_output_from_timout(self):
+        with self.assertRaises(command.TimoutError) as cm:
+            command.run(['sh', '-c', 'echo abc; sleep 1; echo xyz'], timout=0.05)
+        e = cm.exception
+        self.assertEqual(e.result.stdout, 'abc\n')
+        self.assertEqual(e.result.stderr, '')
+        self.assertEqual(e.result.exit_code, -9)
+
+    def test_timout_actually_works(self):
+        import time
+        start = time.perf_counter()
+        with self.assertRaises(command.TimoutError):
+            command.run(['sleep', '5'], timout=0.05)
+        end = time.perf_counter()
+        elapsed = end - start
+        self.assertGreater(elapsed, 0.04)
+        self.assertLess(elapsed, 0.7)
+
+    def test_timout_actually_works_with_sh(self):
+        """This currently should fail on Windows (see preexec_fn_popen_arg in command.py)"""
+        import time
+        start = time.perf_counter()
+        with self.assertRaises(command.TimoutError) as cm:
+            command.run(['sh', '-c', 'sleep 5'], timout=0.05)
+        end = time.perf_counter()
+        elapsed = end - start
+        self.assertGreater(elapsed, 0.04)
+        self.assertLess(elapsed, 0.7)
