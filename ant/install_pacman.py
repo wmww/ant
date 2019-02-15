b84a79caa921b650bbf57759647aeff810087a91
William Wold <wm@wmww.sh>
2019-02-15 12:10:18 -0500

    Add pacman support

---
+++ ant/install_pacman.py
@@ -0,0 +1,20 @@
+import ant
+
+def install_timout():
+    return 3600
+
+def get_installed_check():
+    return ant.Command('pacman', '--version').check_success()
+
+class Install(ant.Ant):
+    def __init__(self, package):
+        self.package = package
+
+    def march(self, queen):
+        return [
+            ant.Command()
+                .with_args('pacman', '-S', self.package.get_name())
+                .with_timout(install_timout())
+                .with_passthrough()
+                .with_sudo()
+        ]
