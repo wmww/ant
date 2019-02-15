28759c3d19694f8ecd9f03d39644ac6b60647fe4
William Wold <wm@wmww.sh>
2019-02-14 23:48:46 -0500

    Disabled timout tests by default

--- util/test/test_command.py
+++ util/test/test_command.py
@@ -3,7 +3,8 @@ import signal
 import util.command as command
 
 # if you have unexplained or intermittent test failures (especially on slower hardware) consider raising these
-default_timout = 0.01 # default time to test timout
+timout_tests_enabled = False
+default_timout = 0.02 # default time to test timout
 default_time_long = default_timout * 100 + 1 # default amount to sleep (which will get interrupted)
 default_timout_tolerance = default_timout * 0.1 + 0.01 # how much longer than timout to accept
 default_python_timout = default_timout * 2 + 0.1 # Python startup is too damn slow
@@ -46,64 +47,79 @@ class TestCommand(unittest.TestCase):
         self.assertEqual(e.result.stderr, '')
         self.assertEqual(e.result.exit_code, 1)
 
-    def test_sleep_short(self):
-        result = command.run(['sleep', str(default_timout)],
-                             timout=default_timout * 2)
-        self.assertEqual(result.stdout, '')
-        self.assertEqual(result.stderr, '')
-        self.assertEqual(result.exit_code, 0)
+    def test_input(self):
+        result = command.run(['cat'], input_str='xyz\n')
+        self.assertEqual(result.stdout, 'xyz\n')
 
-    def test_sleep_timout(self):
+    def disabled_test_sudo(self):
         with self.assertRaises(command.TimoutError) as cm:
-            command.run(['sleep', str(default_time_long)],
-                        timout=default_timout)
+            command.run(['echo', 'abc'], timout=default_timout, sudo=True)
         e = cm.exception
         self.assertEqual(e.result.stdout, '')
-        self.assertEqual(e.result.stderr, '')
-        self.assertEqual(e.result.exit_code, -signal.SIGTERM)
+        self.assertRegex(e.result.stderr, '\\[sudo\\] password for .*:')
+        self.assertEqual(e.result.exit_code, 1)
 
-    def test_output_from_timout(self):
-        with self.assertRaises(command.TimoutError) as cm:
-            command.run(['sh', '-c', 'echo abc; sleep ' + str(default_time_long) + '; echo xyz'],
-                        timout=default_timout)
-        e = cm.exception
-        self.assertEqual(e.result.stdout, 'abc\n')
-        self.assertEqual(e.result.stderr, '')
-        self.assertEqual(e.result.exit_code, -signal.SIGTERM)
+if timout_tests_enabled:
+    class TestCommandTimout(unittest.TestCase):
 
-    def test_output_from_timout_no_raise(self):
-        result = command.run(['sh', '-c', 'echo abc; sleep ' + str(default_time_long) + '; echo xyz'],
-                             timout=default_timout,
-                             ignore_error=True)
-        self.assertEqual(result.stdout, 'abc\n')
-        self.assertEqual(result.stderr, '')
-        self.assertEqual(result.exit_code, -signal.SIGTERM)
+        def test_sleep_short(self):
+            result = command.run(['sleep', str(default_timout)],
+                                 timout=default_timout * 2)
+            self.assertEqual(result.stdout, '')
+            self.assertEqual(result.stderr, '')
+            self.assertEqual(result.exit_code, 0)
 
-    def test_timout_actually_works(self):
-        import time
-        start = time.perf_counter()
-        with self.assertRaises(command.TimoutError):
-            command.run(['sleep', str(default_time_long)],
-                        timout=default_timout)
-        end = time.perf_counter()
-        elapsed = end - start
-        self.assertGreater(elapsed, default_timout)
-        self.assertLess(elapsed, default_timout + default_timout_tolerance)
+        def test_sleep_timout(self):
+            with self.assertRaises(command.TimoutError) as cm:
+                command.run(['sleep', str(default_time_long)],
+                            timout=default_timout)
+            e = cm.exception
+            self.assertEqual(e.result.stdout, '')
+            self.assertEqual(e.result.stderr, '')
+            self.assertEqual(e.result.exit_code, -signal.SIGTERM)
 
-    def test_timout_actually_works_with_child(self):
-        import time
-        start = time.perf_counter()
-        with self.assertRaises(command.TimoutError) as cm:
-            command.run(['sh', '-c', 'sleep ' + str(default_time_long)],
-                        timout=default_timout)
-        end = time.perf_counter()
-        elapsed = end - start
-        self.assertGreater(elapsed, default_timout)
-        self.assertLess(elapsed, default_timout + default_timout_tolerance)
+        def test_output_from_timout(self):
+            with self.assertRaises(command.TimoutError) as cm:
+                command.run(['sh', '-c', 'echo abc; sleep ' + str(default_time_long) + '; echo xyz'],
+                            timout=default_timout)
+            e = cm.exception
+            self.assertEqual(e.result.stdout, 'abc\n')
+            self.assertEqual(e.result.stderr, '')
+            self.assertEqual(e.result.exit_code, -signal.SIGTERM)
 
-    def test_cooperative_child_gets_terminated(self):
-        with self.assertRaises(command.TimoutError) as cm:
-            command.run(['python3', '-c', '''
+        def test_output_from_timout_no_raise(self):
+            result = command.run(['sh', '-c', 'echo abc; sleep ' + str(default_time_long) + '; echo xyz'],
+                                 timout=default_timout,
+                                 ignore_error=True)
+            self.assertEqual(result.stdout, 'abc\n')
+            self.assertEqual(result.stderr, '')
+            self.assertEqual(result.exit_code, -signal.SIGTERM)
+
+        def test_timout_actually_works(self):
+            import time
+            start = time.perf_counter()
+            with self.assertRaises(command.TimoutError):
+                command.run(['sleep', str(default_time_long)],
+                            timout=default_timout)
+            end = time.perf_counter()
+            elapsed = end - start
+            self.assertGreater(elapsed, default_timout)
+            self.assertLess(elapsed, default_timout + default_timout_tolerance)
+
+        def test_timout_actually_works_with_child(self):
+            import time
+            start = time.perf_counter()
+            with self.assertRaises(command.TimoutError) as cm:
+                command.run(['sh', '-c', 'sleep ' + str(default_time_long)],
+                            timout=default_timout)
+            end = time.perf_counter()
+            elapsed = end - start
+            self.assertGreater(elapsed, default_timout)
+            self.assertLess(elapsed, default_timout + default_timout_tolerance)
+
+        def test_cooperative_child_gets_terminated(self):
+            with self.assertRaises(command.TimoutError) as cm:
+                command.run(['python3', '-c', '''
 import time
 import sys
 try:
@@ -113,15 +129,15 @@ try:
 except:
     print('b')
     sys.stdout.flush()
-                '''], timout=default_python_timout)
-        e = cm.exception
-        self.assertEqual(e.result.stdout, 'a\n')
-        self.assertEqual(e.result.stderr, '')
-        self.assertEqual(e.result.exit_code, -signal.SIGTERM)
+                    '''], timout=default_python_timout)
+            e = cm.exception
+            self.assertEqual(e.result.stdout, 'a\n')
+            self.assertEqual(e.result.stderr, '')
+            self.assertEqual(e.result.exit_code, -signal.SIGTERM)
 
-    def test_uncooperative_child_gets_killed(self):
-        with self.assertRaises(command.TimoutError) as cm:
-            result = command.run(['python3', '-c', '''
+        def test_uncooperative_child_gets_killed(self):
+            with self.assertRaises(command.TimoutError) as cm:
+                result = command.run(['python3', '-c', '''
 import time
 import signal
 import sys
@@ -136,20 +152,8 @@ try:
 except:
     print('b')
     sys.stdout.flush()
-                '''], timout=default_python_timout)
-        e = cm.exception
-        self.assertEqual(e.result.stdout, 'a\nc\n')
-        self.assertEqual(e.result.stderr, '')
-        self.assertEqual(e.result.exit_code, -signal.SIGKILL) # Note the SIGKILL instead of SIGTERM, like in the rest
-
-    def test_input(self):
-        result = command.run(['cat'], input_str='xyz\n')
-        self.assertEqual(result.stdout, 'xyz\n')
-
-    def disabled_test_sudo(self):
-        with self.assertRaises(command.TimoutError) as cm:
-            command.run(['echo', 'abc'], timout=default_timout, sudo=True)
-        e = cm.exception
-        self.assertEqual(e.result.stdout, '')
-        self.assertRegex(e.result.stderr, '\\[sudo\\] password for .*:')
-        self.assertEqual(e.result.exit_code, 1)
+                    '''], timout=default_python_timout)
+            e = cm.exception
+            self.assertEqual(e.result.stdout, 'a\nc\n')
+            self.assertEqual(e.result.stderr, '')
+            self.assertEqual(e.result.exit_code, -signal.SIGKILL) # Note the SIGKILL instead of SIGTERM, like in the rest
